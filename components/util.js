8fabb64f9dfa87877427f57c03b059032cda6e9e
army8735 <army8735@qq.com>
2019-03-13 10:59:52 +0800

    feat: remove tracert

--- components/util.js
+++ components/util.js
@@ -42,32 +42,6 @@ function label(text, index, total) {
 }
 
 function tracert(type, appId, url) {
-  my.call('remotelog', {
-    type: 'behavior',
-    bizType: 'FORTUNEAPP',
-    seedId: 'mini-chart',
-    logLevel: 2, // 1 - high, 2 - medium, 3 - low
-    actionId: 'exposure',
-    param1: appId,
-    param2: url,
-    param3: type,
-  });
-
-  const Tracert = new tracertTa({
-    spmAPos: 'a945', // spma位，必填
-    spmBPos: 'b10625', // spmb位，必填
-    bizType: 'FORTUNEAPP', // 业务类型，必填
-    logLevel: 2, // 默认是2
-    chInfo: '', // 渠道
-      mdata: { // 通用的数据，可不传，传了所有的埋点均会带该额外参数
-    },
-    debug: false // 默认是false，传true 则会在发起埋点请求的时候，在控制台输出日志
-  });
-  Tracert.expo('c25441.d47746', {
-    type,
-    appId,
-    url,
-  });
 }
 
 export default {
